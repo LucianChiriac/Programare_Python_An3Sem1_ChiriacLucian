a488748030eb6bafe9918a2488a6a8d144cf0dae
LucianChiriac <35330198+LucianChiriac@users.noreply.github.com>
2022-11-11 12:07:31 +0200

    b

--- Lab5/ex8.py
+++ Lab5/ex8.py
@@ -34,4 +34,25 @@ augmented = print_arguments(suma)
 print(augmented(10,20,30))
 
 
-#%%
+'''
+b) Write a function called multiply_output with 
+one parameter named function. The function will 
+return one new function which returns the output 
+of the function received multiplied by 2.
+Example:
+
+def multiply_by_three(x):
+    return x * 3
+
+augmented_multiply_by_three = multiply_output(multiply_by_three)
+x = augmented_multiply_by_three(10)  # this will return 2 * (10 * 3)
+
+'''
+
+def multiply_output(function):
+    def newfunc(*args,**kwargs):
+        return function(*args,**kwargs)*2
+    return newfunc
+
+example  = multiply_output(suma)
+print(example(10,20,30))
